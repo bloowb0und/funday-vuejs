459f5f08a28c5ed00e951e2e67634dd5a0334dac
m00nler <91968598+m00nler@users.noreply.github.com>
2022-05-17 21:00:04 +0300

    for pupsik

--- funday/src/components/sections/ProjectDetailsWrapper.vue
+++ funday/src/components/sections/ProjectDetailsWrapper.vue
@@ -22,7 +22,7 @@
                                 <strong>Свободных мест: </strong>{{ project.places }}
                               </li>
                               <li>
-                                <strong>Локация: </strong>{{ project.desc }}
+                                <strong>Этажей: </strong>{{ project.floors }}
                               </li>
                             </ul>
                           <btn></btn>
@@ -36,15 +36,25 @@
                         </div>
                     </div>
                   <div class="col-lg-8">
-                    <!-- top bar right -->
                     <div class="top-bar-right-wrapper">
                       <btn @click = "openModalWindow" class="ht-btn ht-btn--default d-inline-block">Забронировать</btn>
                     </div>
                   </div>
-                </div>
+                  <div class="col-12 pt-lg-5">
+                    <div class="row row-5 image-popup">
+                      <a :href="`/assets/img/projects/project-${project.id}/1.jpg`" class="gallery-item single-gallery-thumb col-xl-3 col-lg-4 col-sm-6 col-12 section-space--top--10">
+                        <img :src="`/assets/img/projects/project-${project.id}/1.jpg`"class="img-fluid" alt="thumbnail">
+                        <span class="plus"></span>
+                      </a>
+                      <a :href="`/assets/img/projects/project-${project.id}/2.jpg`" class="gallery-item single-gallery-thumb col-xl-3 col-lg-4 col-sm-6 col-12 section-space--top--10">
+                        <img :src="`/assets/img/projects/project-${project.id}/2.jpg`"class="img-fluid" alt="thumbnail">
+                        <span class="plus"></span>
+                      </a>
+                    </div>
+                  </div>
             </div>
         </div>
-        <!--Projects section end-->
+    </div>
     </div>
 </template>
 
